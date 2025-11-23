2dfca9738e7b7db12b3a4f54d09888a26dc63f27
Fabian Rapp <frapp@3-H-4.42heilbronn.de>
2025-11-23 18:22:00 +0100

    worked on ft_atoi_base

--- src/ft_atoi_base.s
+++ src/ft_atoi_base.s
@@ -23,46 +23,115 @@ ft_isspace:
 	ret
 
 
+;bool valid_base(chae *base_str) {
+;	[x] char mem[256];
+;	[x] memset(mem, 0, 256);
+;
+;	[x] for (int i = 0; base_str[i]; i++) {
+;	 	if (
+;	[ ]			mem[base_str[i]] 
+;	[x]			|| base_str[i] == '+' 
+;	[x]			|| base_str[i] == '-' 
+;	[x]			|| ft_isspace(base_str[i])) {
+;	[x] 		return false
+;	 	}
+;	[ ] 	mem[base_str[i]] = 1;
+;	}
+;	[x] return true;
+;}
+valid_base:
+	push rbx
+	; set rbx as base_str
+	mov rbx, rdi
+
+	;char mem(rcx)[256];
+	sub rsp, 256
+	mov rcx, rsp
+	mov rax, 256
+	;memset(mem, 0, 256)
+.memset_loop:
+	mov byte [rcx], 0
+	dec rax
+	inc rcx
+	test rax, rax
+	jnz .memset_loop
+
+	sub rcx, 256
+
+	xor rdi, rdi; for ft_isspace args zero it the upper bytes
+
+.loop:
+	; c= *base_str
+	mov dil, [rbx]
+	; if !c return true
+	cmp dil, 0
+	je .ret_true
+
+	; if ft_isspace(c) return false
+	push rcx
+	push rdi
+	call ft_isspace
+	pop rcx
+	pop rdi
+	test rax, rax
+	jnz .ret_false
+
+	cmp dil, '-'
+	je .ret_false
+	cmp dil, '+'
+	je .ret_false
+
+;TODO: mem[xyz]
+
+	;base_str++
+	inc rbx
+	jmp .loop
+
+.ret_true:
+	pop rbx
+	add rsp, 256
+	mov rax, 1
+	ret
+.ret_false:
+	pop rbx
+	add rsp, 256
+	mov rax, 0
+	ret
+
 ;int ref_ft_atoi_base(char *str, char *base_str) {
-;	int ret = 0;
-;	int base = strlen(base_str);
-;	int i = 0;
-;	int sign = 1;
-;	char mem[256];
-;	memset(mem, 0, 256);
-;	for (int i = 0; i < base; i++) {
-;		if (mem[base_str[i]] || base_str[i] == '+' || base_str[i] == '-' || ft_isspace(base_str[i])) {
-;			//invalid base
-;			return (0);
-;		}
-;		mem[base_str[i]] = 1;
-;	}
-;	while (ft_isspace(*str)) {
-;		str++;
-;	}
-;	if (str[0] == '-') {
-;		sign = -1;
-;		str++;
-;	} else if (*str == '+') {
-;		str++;
-;	}
-;	for (int i = 0; str[i]; i++) {
-;		int digit = 0;
-;		for (; digit < base; digit++) {
-;			if (str[i] == base_str[digit]) {
-;				ret *= base;
-;				ret += digit;
-;				break ;
-;			}
-;		}
-;		if (digit == base) {
-;			return ret * sign;
-;		}
-;	}
-;	return ret * sign;
+;	[ ] int ret = 0;
+;	[ ] int base = strlen(base_str);
+;	[ ] int i = 0;
+;	[ ] int sign = 1;
+;	[ ] if (!valid_base(base_str)) {
+;	[ ] 	return false;
+;	[ ] }
+;	[ ] while (ft_isspace(*str)) {
+;	[ ] 	str++;
+;	[ ] }
+;	[ ] if (str[0] == '-') {
+;	[ ] 	sign = -1;
+;	[ ] 	str++;
+;	[ ] } else if (*str == '+') {
+;	[ ] 	str++;
+;	[ ] }
+;	[ ] for (int i = 0; str[i]; i++) {
+;	[ ] 	int digit = 0;
+;	[ ] 	for (; digit < base; digit++) {
+;	[ ] 		if (str[i] == base_str[digit]) {
+;	[ ] 			ret *= base;
+;	[ ] 			ret += digit;
+;	[ ] 			break ;
+;	[ ] 		}
+;	[ ] 	}
+;	[ ] 	if (digit == base) {
+;	[ ] 		return ret * sign;
+;	[ ] 	}
+;	[ ] }
+;	[ ] return ret * sign;
 ;}
 ft_atoi_base:
-
+	xor rax, rax
 	ret
 
 
