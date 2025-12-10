8544428db493800968da4fff16ccda09eb4edeea
FabianRapp <fa.rapp@web.de>
2025-12-10 13:57:48 +0100

    implemented sign check

--- src/ft_atoi_base.s
+++ src/ft_atoi_base.s
@@ -2,6 +2,7 @@
 ;int ft_atoi_base(char *str, char *base_str);
 
 global ft_atoi_base
+extern ft_strlen
 section .text
 
 
@@ -40,8 +41,6 @@ ft_isspace:
 ;	[x] return true;
 ;}
 
-;TODO: BUGGED
-; mem[c] dosn't work i think?
 valid_base:
 	push rbx
 	; set rbx as base_str
@@ -115,22 +114,22 @@ valid_base:
 	ret
 
 ;int ref_ft_atoi_base(char *str, char *base_str) {
-;	[ ] int ret = 0;
-;	[ ] int base = strlen(base_str);
+;	[x] int ret = 0;
+;	[x] int base = strlen(base_str);
 ;	[ ] int i = 0;
-;	[ ] int sign = 1;
+;	[x] int sign = 1;
 ;	[x] if (!valid_base(base_str)) {
 ;	[x] 	return false;
 ;	[x] }
 ;	[x] while (ft_isspace(*str)) {
 ;	[x] 	str++;
-;	[ ] }
-;	[ ] if (str[0] == '-') {
-;	[ ] 	sign = -1;
-;	[ ] 	str++;
-;	[ ] } else if (*str == '+') {
-;	[ ] 	str++;
-;	[ ] }
+;	[x] }
+;	[x] if (str[0] == '-') {
+;	[x] 	sign = -1;
+;	[x] 	str++;
+;	[x] } else if (*str == '+') {
+;	[x] 	str++;
+;	[x] }
 ;	[ ] for (int i = 0; str[i]; i++) {
 ;	[ ] 	int digit = 0;
 ;	[ ] 	for (; digit < base; digit++) {
@@ -140,11 +139,11 @@ valid_base:
 ;	[ ] 			break ;
 ;	[ ] 		}
 ;	[ ] 	}
-;	[ ] 	if (digit == base) {
+;	[ ] 	if (digit == base) { // c is not in base_str -> return number
 ;	[ ] 		return ret * sign;
 ;	[ ] 	}
 ;	[ ] }
-;	[ ] return ret * sign;
+;	[x] return ret * sign;
 ;}
 
 ft_atoi_base:
@@ -154,43 +153,96 @@ ft_atoi_base:
 	test rsi, rsi
 	jz .ret_0_0
 
+
 	push rbx ; push old_rbx
 	push rsi ; push base
 	push rdi ; push str
+
 	mov rdi, rsi
 	call valid_base
 	pop rdi ; pop str
 	test rax, rax
 	jz .ret_0_1
 
-	mov rbx, rdi
-	xor rdi, rdi
+	mov rbx, rdi ; -> rbx is now the number-string
 
 .skip_space:
-	mov byte rdi, [rbx]
+	xor rdi, rdi
+	mov dil, [rbx]
+	inc rbx
 
-	;if !*str -> return 0
-	test rdi, rdi
+	test rdi, rdi ; if !*str -> return 0
 	jz .ret_0_1
 
-	inc rbx
 	call ft_isspace
 	test rax, rax
 	jnz .skip_space
 
-	dec rbx
+	dec rbx ; since the above loop increases once too much
+
+	pop rdi; get the base_str for strlen
+	push rdi
+	call ft_strlen
+
 	pop rsi ;pop base
 
+	push r8
+	mov r8, rax ; r8 will now hold the base (strlen(base_str))
 
+	push r9 ; r9 will hold the result
+	xor r9, r9
+
+	push r10 ;r10 will hold the sign for the return result
+	mov r10, 1
+
+	xor rdi, rdi
+	mov dil, [rbx] ; dil is now the current char c
+
+	cmp dil, '+'
+	je .inc_and_loop
+
+	cmp dil, '-'
+	jne .main_loop
+
+	mov r10, -1 ;sign = -1
+
+.inc_and_loop:
+	inc rbx
+	jmp .main_loop
+
+	; ======================================
+	; MAIN LOOP
+	; rbx == current pos in number str
+	; dil(rdi) == current char c ([rbx])
+	; rsi == base_str
+	; r8 == base magnitude
+	; r9 == current number result
+	; ======================================
+.main_loop:
+	xor rdi, rdi
+	mov dil, [rbx] ; dil is now the current char c
+
+	test rdi, rdi
+	jz .ret_result
+
+
+; ====================================
+; success returns
+.ret_result:
+	imul r9, r10 ; result = result * sign
+	mov rax, r9
+	pop r10
+	pop r9
+	pop r8
 .ret:
-	xor rax, rax
-	inc rax
-	pop rbx
+	pop rbx ;pop old_rbx
 	ret
 
+; ====================================
+; error retruns -> return 0
 .ret_0_1:
 	pop rsi ;pop base
-	pop rbx
+	pop rbx ;pop old_rbx
 .ret_0_0:
 	xor rax, rax
 	ret
