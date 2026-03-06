fe7e3d7fbb6f3e8ce77fd2d1e096889d11bfc80d
FabianRapp <fa.rapp@web.de>
2026-03-06 14:35:02 +0100

    first working sort

--- src/ft_list_sort_bonus.s
+++ src/ft_list_sort_bonus.s
@@ -29,35 +29,89 @@ section .text
 ft_list_sort:
 	; if (!head) return
 	test rdi, rdi
-	jz .return
-	mov rdi, [rdi]
-	test rdi, rdi
-	jz .return
+	jz .return1
 
-	mov rdx, rsi ; rdx = cmp
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+	sub rsp, 16
+
+	mov r12, rsi ; r12 = cmp
+
+	mov rdi, [rdi] ; prep rdi as ft_list_size args = *head
+	mov r14, rdi ; r14 = *head
+	test r14, r14
+	jz .return2
 
 	call ft_list_size
+	mov r13, rax ; r13 = unsorted_len
+	mov rbx, r13
 
-.outer_loop: ; while (len > 1)
-	dec rax
-	test rax, rax
-	jz .return
+.outer_loop: ; while (true) {
+	dec r13
+	test r13, r13 ; if(!unsorted_len)
+	jz .return2 ; return
 
-	mov rdx, rax ; inner_loop_iters = len
-.inner_loop:
+	mov r15, r13 ; r15 = inner_loop_iters = unsorted_len
 
-;todo: cmp call can alter regs
-	; cmp(cur, next); todo: cmp(cur->data, next->data)
-	mov rsi, [rdi + 8]
-	call rdx
+	mov [rsp], r14 ; [rsp] = cur
+
+	mov rax, r14
+	mov rax, [rax + 8]
+	mov [rsp + 8], rax ; [rsp + 8] = next;
+
+.inner_loop: ; do {
+
+	mov rax, [rsp]
+	mov rdi, [rax] ; rdi = cur->data
+	mov rax, [rsp+ 8]
+	mov rsi, [rax] ; rsi = next->data
+
+	push rdi
+	push rsi
+	; cmp(cur->data, next->data)
+	call r12
+
+	pop rsi
+	pop rdi
+
+	test eax, eax
+	jle .next_iter; if <= 0 jmp
+
+	mov rcx, [rsp]
+	mov [rcx], rsi
+
+	mov rcx, [rsp + 8]
+	mov [rcx], rdi
 
 
-	dec rdx
-	test rdx, rdx
+.next_iter:
+;	cur = next;
+	mov rax, [rsp + 8] ; rax = [rsp + 8] = next
+	mov [rsp], rax ; cur = [rsp] = next
 
+;	next = cur->next;
+	mov rax, [rsp] ; rax = cur
+	mov rax, [rax + 8] ; rax = [cur + 8] = cur->next
+	mov [rsp + 8], rax ; next = [rsp + 8] = rax
+
+	dec r15 ; inner_loop_iters--
+	test r15, r15; } while (inner_loop_iters)
 	jnz .inner_loop
-	jmp .outer_loop
+
+	jmp .outer_loop ; }
 
 
-.return:
+.return1:
+	ret
+
+.return2:
+	add rsp, 16
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
 	ret
