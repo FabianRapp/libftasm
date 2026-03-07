5561e399881bc753e9fdb9a157c9ffc11d3056e6
FabianRapp <fa.rapp@web.de>
2026-03-07 21:01:22 +0100

    started performance improvments for strlen

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -3,14 +3,39 @@
 section .text
 	global ft_strlen
 
+
 ft_strlen:
 	xor rax, rax
-.loop:
-	cmp byte [rdi], 0
-	je .ret
-	inc rax
-	inc rdi
-	jmp .loop
+
+%macro loop1 0
+	%rep 32
+		cmp byte [rdi + rax], 0
+		je .ret
+		inc rax
+	%endrep
+%endmacro
+
+
+; do basic checks with an unrolled loop for small strings so there is little overhead
+.loop1:
+	loop1
+	jmp .loop1
+	; todo: aligment for simd
+	; todo: simd
+
 .ret:
 	ret
+
+
+
+;ft_strlen:
+;	xor rax, rax
+;.loop:
+;	cmp byte [rdi], 0
+;	je .ret
+;	inc rax
+;	inc rdi
+;	jmp .loop
+;.ret:
+;	ret
 section .note.GNU-stack noalloc noexec nowrite progbits
